c157344d17a6bf9c636d00d505c9630284288c5d
rafiadiningrat <80728200+rafiadiningrat@users.noreply.github.com>
2022-03-24 22:55:45 +0700

    DataStrapi

--- Kelompok/PLN-Postgre/be/my-project/config/database.js
+++ Kelompok/PLN-Postgre/be/my-project/config/database.js
@@ -8,7 +8,7 @@ module.exports = ({ env }) => ({
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'db_pln'),
       user: env('DATABASE_USERNAME', 'postgres'),
-      password: env('DATABASE_PASSWORD', 'postgres'),
+      password: env('DATABASE_PASSWORD', 'rafiadiningrat53'),
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
