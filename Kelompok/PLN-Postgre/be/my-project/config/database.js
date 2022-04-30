979c12dee34c70a8b0e07c26fa8d650d167a77a2
faishalm22 <faishal.muhammad.tif20@polban.ac.id>
2022-04-30 22:37:31 +0700

    update

--- Kelompok/PLN-Postgre/be/my-project/config/database.js
+++ Kelompok/PLN-Postgre/be/my-project/config/database.js
@@ -8,11 +8,7 @@ module.exports = ({ env }) => ({
       port: env.int('DATABASE_PORT', 5432),
       database: env('DATABASE_NAME', 'db_pln'),
       user: env('DATABASE_USERNAME', 'postgres'),
-<<<<<<< HEAD
-      password: env('DATABASE_PASSWORD', 'virgo2'),
-=======
       password: env('DATABASE_PASSWORD', 'irfannoor123'),
->>>>>>> e4b48e135cd59c627fb0bb3012c4be8b450a0651
       ssl: env.bool('DATABASE_SSL', false),
     },
   },
