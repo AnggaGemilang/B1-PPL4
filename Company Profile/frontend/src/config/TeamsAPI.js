f60bb749a36a0081c56004f7ad036b21c3a5b647
AnggaGemilang <anggagemilang12@gmail.com>
2022-05-14 15:15:36 +0700

    atur branch main

--- Company Profile/frontend/src/config/TeamsAPI.js
+++
@@ -1,8 +0,0 @@
-import api from ".";
-
-export default {
-    get: () => api.get("/teams?populate=*").then((res) => res.data),
-    add: () => api.get("/teams?populate=*").then((res) => res.data),
-    delete: () => api.get("/teams?populate=*").then((res) => res.data),
-    edit: () => api.get("/teams?populate=*").then((res) => res.data)
-}
