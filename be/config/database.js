2ffb538c8843cfed02eb01fc40e831091b73bff5
faishalm22 <77891212+faishalm22@users.noreply.github.com>
2022-07-06 21:56:09 +0700

    Update database.js

--- be/config/database.js
+++ be/config/database.js
@@ -6,7 +6,7 @@ module.exports = ({ env }) => ({
     connection: {
       host: env('DATABASE_HOST', 'localhost'),
       port: env.int('DATABASE_PORT', 5432),
-      database: env('DATABASE_NAME', 'db_pln'),
+      database: env('DATABASE_NAME', 'db_fit_proper'),
       user: env('DATABASE_USERNAME', 'postgres'),
       password: env('DATABASE_PASSWORD', 'irfannoor123'),
       ssl: env.bool('DATABASE_SSL', false),
